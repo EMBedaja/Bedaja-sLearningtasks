2507843a6efa7a93cce9895899d9069d0bf237d4
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2022-02-21 09:16:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <title> My Home Pagae </title>
   </head>
   <body>
-  <h1> Welcome to My Website! I am Ella Marie! </h1>
+  <h1> Welcome to My Website! I am Ella Marie Bedaja! </h1>
   <ul>
   <li> <a href="index.php"> HOME </a> </li>
   <li> <a href="/LT2/index.html"> LEARNING TASK 2 </a></li>
