4cbf2ed5069dfdf4f1978120d35c2ca6f99a959e
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2022-02-21 08:57:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title> My Home Pagae </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Ella Marie! </h1>
+  <ul>
+  <li> <a href="index.php"> HOME </a> </li>
+  <li> <a href="/LT2/index.html"> LEARNING TASK 2 </a></li>
+  </ul>
+  </body>
+</html>
