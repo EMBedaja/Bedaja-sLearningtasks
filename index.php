be43c7c1724154f288b208d8981f8a46df1eb6e1
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2022-02-21 11:14:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <h1> Welcome to My Website! I am Ella Marie Bedaja! </h1>
   <ul>
   <li> <a href="index.php"> HOME </a> </li>
-  <li> <a href="/LT2/index.html"> LEARNING TASK 2 </a></li>
+  <li> <a href="/LT2/HOME.html"> LEARNING TASK 2 </a></li>
   </ul>
   </body>
 </html>
