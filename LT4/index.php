151e08f1ea2bf45c3aa08fd55a3129c5fd899fe9
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2022-04-04 21:43:18 +0800

    Create index.php

---
+++ LT4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Ella Marie Bedaja! </h1>
+  <ul>
+  <li> <a href="index.php"> HOME </a> </li>
+    <li> <a href="/LT2/HOME.html"> LEARNING TASK 2 </a></li>
+    <li> <a href="/LT3/index.html"> LEARNING TASK 3 </a></li>
+    <li> <a href="/LT4/index.html"> LEARNING TASK 4 </a></li>
+    <li> <a href="/CLE1MT/index.html"> CLE1MT </a></li>
+  
+  </ul>
+  </body>
+</html>
