924c13dece723df2d732eec2d8ecb69a9f67e290
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2022-05-04 12:34:09 +0800

    Update script.js

--- LT5/js/script.js
+++ LT5/js/script.js
@@ -1,4 +1,9 @@
 /eslint-env browser /
 function setNewImage () {
-    document.getElementById('jah').src = "images/jah.gif";
+    document.getElementById('jah').src="images/jah.gif";
+    document.getElementById('ken').src="images/ken.gif";
+    document.getElementById('pablo').src="images/pablo.gif";
+    document.getElementById('stell').src="images/stell.gif";
+    document.getElementById('josh').src="images/josh.gif";
+} 
 } 
